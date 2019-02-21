6056316fd90b970add192344c29346b96863aabd
AmgadMansour <amgd.a7md@gmail.com>
2019-02-20 17:05:20 -0800

    Reading video + intra coding & decoding

--- main.m
+++ main.m
@@ -1,4 +1,9 @@
 clc;
 clear all;
 close all
-run('gui.m')
+pwd
+%run('gui.m')
+cd '/Users/macbookair/Desktop/Multimedia Systems/Ass2/vc';
+
+vidPath = '/Users/macbookair/Desktop/Multimedia Systems/Ass2/vc/SampleVideo_640x360_1mb.mp4';
+readVideo(vidPath);
